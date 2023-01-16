f518607c523549c7a502fd2275d8a4c8d74f3961
Opemipo Bolaji <96318373+opemipobolaji@users.noreply.github.com>
2023-01-16 21:22:45 +0100

    Highlighting over lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
+import { SfNode } from './models/sf-node';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sf-highlight';
+
+  allNodes: SfNode[] = [
+    new SfNode('1', 'First Description'),
+    new SfNode('2', 'Second Description'),
+    new SfNode('3', 'Third Description'),
+    new SfNode('4', 'Fourth Description'),
+  ];
+
+  nodesProcessed: SfNode[] = [];
+
+  startingNode?: SfNode | null;
+  endingNode?: SfNode | null;
+
+  onmousedown($event: MouseEvent, node: SfNode) {
+    this.startingNode = node;
+    this.endingNode = null;
+    this.nodesProcessed = [node];
+  }
+
+  onmouseover($event: MouseEvent, node: SfNode) {
+    if (!this.startingNode) {
+      return ;
+    }
+
+    const onTheList = this.isANodeToProcess(node);
+
+    if (onTheList) {
+      const nextNode = this.findNextNodeToProcess(node);
+      if (nextNode) {
+        this.removeNodeFromProcessing(nextNode);
+      }
+    }
+
+    if (!onTheList) {
+      this.nodesProcessed.push(node);
+    }
+  }
+
+  onmouseup($event: MouseEvent, node: SfNode) {
+    if (!this.startingNode) {
+      return ;
+    }
+
+    if (this.nodesProcessed.length === 1) {
+      this.removeNodeFromProcessing(node)
+    }
+
+    this.endingNode = node;
+    this.startingNode = null;
+  }
+
+  private isANodeToProcess(node: SfNode): SfNode | undefined {
+    return this.nodesProcessed.find(lNode => lNode.id === node.id);
+  }
+
+  private removeNodeFromProcessing(node: SfNode) {
+    this.nodesProcessed.splice(this.nodesProcessed.indexOf(node), 1);
+  }
+
+  private findNextNodeToProcess(node: SfNode): SfNode | undefined {
+    return this.nodesProcessed.at(this.nodesProcessed.indexOf(node) + 1);
+  }
 }
